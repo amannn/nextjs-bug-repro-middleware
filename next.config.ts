ae2ae6921245a902693018fd63d6218bc45f84e1
Jan Amann <jan@amann.work>
2025-01-22 10:59:06 +0100

    setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next';
+import createBundleAnalyzer from '@next/bundle-analyzer';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withBundleAnalyzer = createBundleAnalyzer({
+  enabled: true
+});
 
-export default nextConfig;
+const nextConfig: NextConfig = {};
+
+export default withBundleAnalyzer(nextConfig);
